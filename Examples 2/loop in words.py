0768256aa418e27f6771807848905fb8fd044d69
AFAR <afavendano@hotmail.com>
2018-12-10 20:48:26 -0600

    created new file with 3 examples

---
+++ Examples 2/loop in words.py
@@ -0,0 +1,14 @@
+# Measure some strings:
+# words = ['Test1', 'window', 'defenestrate']
+# for w in words:
+#     print(w, len(w))
+
+# Example 2
+# u_entered_words = input("Please Enter a word: ")
+# print(u_entered_words,len(u_entered_words))
+
+
+# Example 3
+a = ['Word 0', 'Word 1', 'Word 2', 'Word 3', 'Word 4']
+for i in range(len(a)):
+        print(i, a[i])
